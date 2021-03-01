61fdfeca06ad69d2a3d28d8d3ec0fab9c6fd09d7
keithfilegar <keith.filegar@gmail.com>
2021-03-01 14:54:41 -0800

    create db seeds

---
+++ seeds/seed.folders.sql
@@ -0,0 +1,6 @@
+INSERT INTO folders (name)
+VALUES
+        ('Game of Thrones'),
+        ('Breaking Bad'),
+        ('Better Call Saul'),
+        ('SpongeBob');
---
+++ seeds/seed.notes.sql
@@ -0,0 +1,9 @@
+INSERT INTO notes(name, folderId, content)
+VALUES
+    ('Jon Snow', 1, 'KING IN THE NORTH'),
+    ('Arya Startk', 1, 'A girl has no name'),
+    ('Walter White', 2, 'Professional chef'),
+    ('Jesse Pinkman', 2, 'Tragic hero'),
+    ('Saul Goodman', 3, 'Attorney at law'),
+    ('SpongeBob SquarePants', 4, 'Professional chef'),
+    ('Patrick Star', 4, 'No, this is Patrick');
