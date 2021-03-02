28c397b6fab549e33a42cb5194fbdc1081f6bcdd
keithfilegar <keith.filegar@gmail.com>
2021-03-02 11:16:24 -0800

    GET /api/folders successfully returning all folders

---
+++ src/folders/folders-router.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const express = require('express')
+const xss = require('xss')
+const FoldersService = require('./folders-service')
+
+const foldersRouter = express.Router()
+const jsonParser = express.json()
+
+const serializeFolder = folder => ({
+    id: folder.id,
+    name: xss(folder.name)
+})
+
+foldersRouter
+    .route('/')
+    .get((req, res, next) => {
+        FoldersService.getAllFolders(
+            req.app.get('db')
+        )
+        .then(folders => {
+            res.json(folders.map(serializeFolder))
+        })
+        .catch(next)
+    })
+
+module.exports = foldersRouter
