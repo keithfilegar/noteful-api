2722ca173a1a86712d8cd41e69353e8c78e2ee69
keithfilegar <keith.filegar@gmail.com>
2021-03-02 14:10:49 -0800

    add POST /api/folders

--- src/folders/folders-router.js
+++ src/folders/folders-router.js
@@ -22,5 +22,29 @@ foldersRouter
         })
         .catch(next)
     })
+    .post(jsonParser, (req, res, next) => {
+        const { name } = req.body
+        const newFolder = { name }
+
+        if(req.body.name === null) {
+            return res.status(400).json({
+                error: {
+                    message: `Request body must contain 'name'`
+                }
+            })
+        }
+
+        FoldersService.insertFolder(
+            req.app.get('db'),
+            newFolder
+        )
+        .then(folder => {
+            res
+            .status(201)
+            .location(path.posix.join(req.originalUrl, `/${folder.id}`))
+            .json(serializeFolder(folder))
+        })
+        .catch(next)
+    })
 
 module.exports = foldersRouter
